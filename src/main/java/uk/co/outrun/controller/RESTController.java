975e271ffec82ad73afe4798b87b663ec883fd3d
feebeebee <victoria@maciver.co.uk>
2020-09-22 14:43:53 +0100

    Add cross origin support for the vue app

--- src/main/java/uk/co/outrun/controller/RESTController.java
+++ src/main/java/uk/co/outrun/controller/RESTController.java
@@ -1,6 +1,7 @@
 package uk.co.outrun.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,8 @@ import uk.co.outrun.service.DogService;
 
 import java.util.List;
 
+// TODO - make this configurable
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class RESTController {
 
