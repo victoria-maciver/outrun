e73890bb03845aa1c48208f20acf6779efb2b4a4
feebeebee <victoria@maciver.co.uk>
2020-10-16 17:12:28 +0100

    Add server port for tomcat to run on 8081

--- src/main/java/uk/co/outrun/Main.java
+++ src/main/java/uk/co/outrun/Main.java
@@ -1,24 +1,15 @@
 package uk.co.outrun;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import uk.co.outrun.repository.DogRepository;
-
-
 
 @SpringBootApplication
 public class Main implements CommandLineRunner {
 
-//    @Autowired
-//    private DogRepository repository;
-
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
 
     @Override
     public void run(String... args) {
